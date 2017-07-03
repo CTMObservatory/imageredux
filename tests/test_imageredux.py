7289a676865a757037e1bf77b14260888884697e
Martin Beroiz <martinberoiz@yahoo.com>
2017-07-03 16:35:33 -0500

    Added template for tests

---
+++ tests/test_imageredux.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestAlign(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_examplefunction(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
