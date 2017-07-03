25587e1eb1a82a4b5b8b9f7cef030dd6b761cc8f
Martin Beroiz <martinberoiz@yahoo.com>
2017-07-03 16:21:27 -0500

    Added template for imageredux script

---
+++ imageredux.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017-2018, Martin Beroiz, Richard Camuccio, Juan Garcia,
+# Pamela Lara, Moises Castillo
+# All rights reserved.
+
+
+def examplefunction(arg1, arg2):
+    pass
+
+
+if __name__ == '__main__':
+    print("Hello World!")
