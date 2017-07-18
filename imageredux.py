5a412e7e27f67a547c7b3dea6768e83d7266f382
Juan L Garcia <juanlg.x@gmail.com>
2017-07-18 14:36:37 -0500

    added doDarkCombination

--- imageredux.py
+++ imageredux.py
@@ -9,6 +9,11 @@
 def examplefunction(arg1, arg2):
     print("Hello World!")
 
+def doDarkComb (darklist):
+	"This will combine darks to mke a darkmaster"
+	darkmaster = ccdproc.combine(darklist, output_file="darkmaster.fits", method="median", unit=u.adu)
+	
+	return 'darkmaster.fits'
 
 if __name__ == '__main__':
     examplefunction(2, 3)
