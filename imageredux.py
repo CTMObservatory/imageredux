0494cbd4e9c997cbe2005053974b945fa181c196
Juan L Garcia <juanlg.x@gmail.com>
2017-07-19 14:56:30 -0500

    need to add an overwrite

--- imageredux.py
+++ imageredux.py
@@ -4,15 +4,16 @@
 # Copyright (c) 2017-2018, Martin Beroiz, Richard Camuccio, Juan Garcia,
 # Pamela Lara, Moises Castillo
 # All rights reserved.
-
+import ccdproc
+from astropy import units as u
 
 def examplefunction(arg1, arg2):
     return "Hello World!"
 
 def doDarkComb (darklist):
 	"This will combine darks to mke a darkmaster"
-	darkmaster = ccdproc.combine(darklist, output_file="darkmaster.fits", method="median", unit=u.adu)
-	return 'darkmaster.fits'
+	darkmaster = ccdproc.combine(darklist, output_file="darkmaster.fits", method="median", unit=u.adu, clobber=True)
+	return darkmaster
 
 if __name__ == '__main__':
     print(examplefunction(2, 3))
