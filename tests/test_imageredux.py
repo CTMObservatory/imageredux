38d0f52b9085b08c9597338f489f048513d68ce0
Martin Beroiz <martinberoiz@gmail.com>
2017-07-19 13:13:44 -0500

    Added test for doDarkComb

--- tests/test_imageredux.py
+++ tests/test_imageredux.py
@@ -1,20 +1,32 @@
-import unittest
 import sys
 import os
-
-sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import imageredux
+import unittest
+from astropy.io import fits
 
 
 class TestImageRedux(unittest.TestCase):
     def setUp(self):
-        pass
+        from numpy.random import normal
+        num_test_dark_files = 3
+        self.darkfilenames = []
+        for i in range(num_test_dark_files):
+            hdu = fits.PrimaryHDU(data=normal(loc=100, scale=5, size=(10, 10)))
+            darkname = "dark_test_{:02d}.fits".format(i)
+            self.darkfilenames.append(darkname)
+            hdu.writeto(darkname)
 
     def tearDown(self):
-        pass
+        for afile in self.darkfilenames:
+            os.remove(afile)
 
     def test_examplefunction(self):
         self.assertEqual(imageredux.examplefunction(1, 2), "Hello World!")
+
+    def test_doDarkComb(self):
+        darkmaster_fname = imageredux.doDarkComb(self.darkfilenames)
+        self.assertTrue(os.path.exists("darkmaster_fname"))
 
 
 if __name__ == "__main__":
