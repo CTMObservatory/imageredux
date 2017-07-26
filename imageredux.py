861e317b2d9355632ca6349b75af514e7285ca41
moemyself3 <moemyself3@gmail.com>
2017-07-26 17:30:30 -0500

    First Draft of Image Reduction

--- imageredux.py
+++ imageredux.py
@@ -4,6 +4,9 @@
 # Copyright (c) 2017-2018, Martin Beroiz, Richard Camuccio, Juan Garcia,
 # Pamela Lara, Moises Castillo
 # All rights reserved.
+#import numpy as np
+from astropy import units as u
+import ccdproc
 
 
 def examplefunction(arg1, arg2):
@@ -14,5 +17,19 @@ def doDarkComb (darklist):
 	darkmaster = ccdproc.combine(darklist, output_file="darkmaster.fits", method="median", unit=u.adu)
 	return 'darkmaster.fits'
 
+# This Function takes a single image and reduces using dark and flat master.
+def redux(image, masterDark, masterFlat):
+    print(image)
+    print(masterDark)
+    print(masterFlat)
+    reduxFile = ccdproc.CCDData.read(image,unit="adu") # Read file
+    masterDark = ccdproc.CCDData.read(masterDark, unit="adu")
+    masterFlat = ccdproc.CCDData.read(masterFlat, unit="adu")
+    reduxFile = ccdproc.subtract_dark(reduxFile, masterDark, exposure_time = 30, exposure_unit = u.s) # Subtract Dark
+    reduxFile = ccdproc.flat_correct(reduxFile, masterFlat) # Divide by Flat
+    # Write fits file
+    return reduxFile.write(image+'_redux.fits')
+
 if __name__ == '__main__':
     print(examplefunction(2, 3))
+    redux('hatp27.fit','master_dark.fit','master_flat.fit')
