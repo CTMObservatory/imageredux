0838a7724e33127d6e23a5d7e6e0b63fb7f70f06
moemyself3 <moemyself3@gmail.com>
2017-09-01 17:22:24 -0500

    Added checking and skipping features

--- imageredux.py
+++ imageredux.py
@@ -108,10 +108,13 @@ def do_calibrate(object_list, master_flat, master_dark, object_name, cal_frame_d
     """
 
     cal_dir = "cal_{}".format(object_name)
+    check_path = os.path.join(cal_frame_dir, cal_dir)
 
-    if not os.path.exists(cal_dir):
-        os.makedirs(os.path.join(cal_frame_dir, cal_dir))
+    if not os.path.exists(check_path):
+        os.makedirs(check_path)
 
+    # Calibration begins if directory cal_object exists and is empty
+    if not os.listdir(check_path):
         for item in object_list:
 
             frame = os.path.split(item)[1]
@@ -120,17 +123,23 @@ def do_calibrate(object_list, master_flat, master_dark, object_name, cal_frame_d
             # Read CCDData object
             object_frame = ccdproc.fits_ccddata_reader(item, unit="u.adu")
 
-            log.write("<OUTPUT> Subtracting dark from " + str(frame)+"\n")
-            # Subtract dark from object
-            object_min_dark = ccdproc.subtract_dark(object_frame, master_dark, data_exposure=object_frame.header["exposure"]*u.second, dark_exposure=master_dark.header["exposure"]*u.second, scale=True)
+            # Check if object frame is same size as master frames
+            if not object_frame.shape == master_dark.shape:
+                log.write("<STATUS> Skipping object calibration... Object frame is not same shape as Master frames\n")
+                print("<STATUS> Skipping object calibration... Object frame is not same shape as Master frames")
+                break
+            else:
+                log.write("<OUTPUT> Subtracting dark from " + str(frame)+"\n")
+                # Subtract dark from object
+                object_min_dark = ccdproc.subtract_dark(object_frame, master_dark, data_exposure=object_frame.header["exposure"]*u.second, dark_exposure=master_dark.header["exposure"]*u.second, scale=True)
 
-            log.write("<OUTPUT> Dividing " + str(frame) + " by flat"+"\n")
-            # Divide object by flat
-            cal_object_frame = ccdproc.flat_correct(object_min_dark, master_flat)
+                log.write("<OUTPUT> Dividing " + str(frame) + " by flat"+"\n")
+                # Divide object by flat
+                cal_object_frame = ccdproc.flat_correct(object_min_dark, master_flat)
 
-            log.write("<OUTPUT> Writing object " + str(frame) + " to disk"+"\n")
-            # Write calibrated object to disk
-            ccdproc.fits_ccddata_writer(cal_object_frame, cal_frame_dir+"/"+cal_dir+"/cal-"+str(frame))
+                log.write("<OUTPUT> Writing object " + str(frame) + " to disk"+"\n")
+                # Write calibrated object to disk
+                ccdproc.fits_ccddata_writer(cal_object_frame, cal_frame_dir+"/"+cal_dir+"/cal-"+str(frame))
 
 
 def do_file_list():
@@ -192,40 +201,43 @@ def main():
         log.write("<OUTPUT> dark_list = {}\n".format(dark_list))
         log.write("<OUTPUT> flat_list = {}\n".format(flat_list))
 
-        # Create directory to save masters
-        master_frame_dir = os.path.join(_OUT_DIR, anight_base, "master_frames")
-        log.write("<OUTPUT> master_frame_dir = {}\n".format(master_frame_dir))
-        if not os.path.exists(master_frame_dir):
-            log.write("<OUTPUT> not os.path.exists(master_frame_dir) = " + str(not os.path.exists(master_frame_dir))+"\n")
-            os.makedirs(master_frame_dir)
+        if dark_list and flat_list:
+            # Create directory to save masters
+            master_frame_dir = os.path.join(_OUT_DIR, anight_base, "master_frames")
+            log.write("<OUTPUT> master_frame_dir = {}\n".format(master_frame_dir))
+            if not os.path.exists(master_frame_dir):
+                log.write("<OUTPUT> not os.path.exists(master_frame_dir) = " + str(not os.path.exists(master_frame_dir))+"\n")
+                os.makedirs(master_frame_dir)
 
-        # Create master calibration frames
-        master_dark = do_dark_combine(dark_list, master_frame_dir)
-        master_flat = do_flat_combine(flat_list, master_dark, master_frame_dir)
+            # Create master calibration frames
+            master_dark = do_dark_combine(dark_list, master_frame_dir)
+            master_flat = do_flat_combine(flat_list, master_dark, master_frame_dir)
 
-        # Create list of object directories
-        obj_dirs = [f for f in os.listdir(anight)
-                    if os.path.isdir(os.path.join(anight, f)) and
-                    f not in ['bias', 'dark', 'flat']]
+            # Create list of object directories
+            obj_dirs = [f for f in os.listdir(anight)
+                        if os.path.isdir(os.path.join(anight, f)) and
+                        f not in ['bias', 'dark', 'flat']]
 
-        log.write("<OUTPUT> obj_dirs = {}\n".format(obj_dirs))
-        log.write("<OUTPUT> (Bool) dir 'master_frame_dir' exists > {}\n".format(os.path.exists("master_frame_dir")))
+            log.write("<OUTPUT> obj_dirs = {}\n".format(obj_dirs))
+            log.write("<OUTPUT> (Bool) dir 'master_frame_dir' exists > {}\n".format(os.path.exists("master_frame_dir")))
 
-        # Create directory to save calibrated objects
-        cal_frame_dir = os.path.join(_OUT_DIR, anight_base, "cal_frames")
-        log.write("<OUTPUT> cal_frame_dir = {}\n".format(cal_frame_dir))
-        if not os.path.exists(cal_frame_dir):
-             log.write("<OUTPUT> not os.path.exists(cal_frame_dir) = {}\n".format(not os.path.exists(cal_frame_dir)))
-             os.makedirs(cal_frame_dir)
+            # Create directory to save calibrated objects
+            cal_frame_dir = os.path.join(_OUT_DIR, anight_base, "cal_frames")
+            log.write("<OUTPUT> cal_frame_dir = {}\n".format(cal_frame_dir))
+            if not os.path.exists(cal_frame_dir):
+                 log.write("<OUTPUT> not os.path.exists(cal_frame_dir) = {}\n".format(not os.path.exists(cal_frame_dir)))
+                 os.makedirs(cal_frame_dir)
 
-        # Calibrate object frames
-        for obj in obj_dirs:
-            log.write("<OUTPUT> obj = {}\n".format(obj))
-            object_list = glob.glob(os.path.join(anight, obj, "*.fit*"))
-            log.write("<OUTPUT> object_list = {}\n".format(object_list))
-            print("<STATUS> Running redux on {}".format(object_list))
-            do_calibrate(object_list, master_flat, master_dark, obj, cal_frame_dir)
-
+            # Calibrate object frames
+            for obj in obj_dirs:
+                log.write("<OUTPUT> obj = {}\n".format(obj))
+                object_list = glob.glob(os.path.join(anight, obj, "*.fit*"))
+                log.write("<OUTPUT> object_list = {}\n".format(object_list))
+                print("<STATUS> Running redux on {}".format(object_list))
+                do_calibrate(object_list, master_flat, master_dark, obj, cal_frame_dir)
+        else:
+            log.write("<OUTPUT> Skipping directory: {}\n".format(anight))
+            print("<STATUS> Skipping directory: {}".format(anight))
 
 if __name__ == '__main__':
 
